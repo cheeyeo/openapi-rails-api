2cb6f0ebbe4f988bfe0217c023dac9efb4e90e5e
Chee Yeo <chee@compose.io>
2018-07-21 16:24:46 +0100

    Add option for Rails.application.routes.default_url_options for using url helpers in AMS

--- config/initializers/active_model_serializer.rb
+++ config/initializers/active_model_serializer.rb
@@ -1,1 +1,4 @@
 ActiveModel::Serializer.config.adapter = ActiveModelSerializers::Adapter::JsonApi
+
+# Refer to <active_model_serializers_gem>/lib/active_model_serializers/railitie.rb line 18; AMS sets the SerializationContext.default_url_options = Rails.application.routes.default_url_options so need to set it per env
+Rails.application.routes.default_url_options = Rails.application.config.action_mailer.default_url_options
