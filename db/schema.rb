927b8f3cc51ed52329220aef70ce70aa7902dcfa
Chee Yeo <chee@compose.io>
2018-07-21 17:21:01 +0100

    Add initial User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_13_151632) do
+ActiveRecord::Schema.define(version: 2018_07_20_192932) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
